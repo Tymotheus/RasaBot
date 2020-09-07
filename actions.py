db04eb5656fe8ae5be8950c0de5e225387b1bb50
Teemo <34406602+Tymotheus@users.noreply.github.com>
2020-09-07 16:28:02 +0200

    Update actions.py

--- actions.py
+++ actions.py
@@ -10,9 +10,6 @@ from __future__ import unicode_literals
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-
 from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -25,13 +22,8 @@ from rasa_sdk.forms import FormAction
 
 import logging
 import json
+import random
 
-# We use the medicare.gov database to find information about 3 different
-# healthcare facility types, given a city name, zip code or facility ID
-# the identifiers for each facility type is given by the medicare database
-# xubh-q36u is for hospitals
-# b27b-2uc7 is for nursing homes
-# 9wzi-peqs is for home health agencies
 
 logger = logging.getLogger(__name__)
 
@@ -43,13 +35,98 @@ class Give_Trivia(Action):
         return "give_trivia"
 
     def run(self, dispatcher, tracker, domain):
-        print(tracker.get_slot("trivia_types"))
+        print(tracker.get_slot("trivia_type"))
         #converting json format reply into python dictionary object
         request = json.loads(requests.get('https://opentdb.com/api.php?amount=1').text)
         print(request)
         trivia = request['results'][0]['question']
-        answer = request['results'][0]['correct_answer']
-        #dispatcher.utter_message(trivia + '\n \n' + answer)
+        #converting unconverted apostrophes, ampersands, quotation marks
+        trivia = (((trivia.replace("&quot;", '"')).replace("&amp", "&")).replace("&#039;","'")).replace("&eacute;", "é")
+        correct_answer = request['results'][0]['correct_answer']
+        correct_answer = (((correct_answer.replace("&quot;", '"')).replace("&amp", "&")).replace("&#039;","'")).replace("&eacute;", "é")
+        #getting a list of all possible answers and randomizing it
+        all_answers = request['results'][0]['incorrect_answers']
+        for answ in all_answers:
+            answ = (((answ.replace("&quot;", '"')).replace("&amp", "&")).replace("&#039;","'")).replace("&eacute;", "é")
+        all_answers.append(correct_answer)
+        random.shuffle(all_answers)
+
+        buttons = []
+        for answ in all_answers:
+            if answ == correct_answer:
+                intent = "answer_correct"
+            else :
+                intent = "answer_incorrect"
+            print("Banana")
+            buttons.append(
+
+                {"title": "{}".format(answ),
+                "payload": "{}".format(intent)
+                }
+            )
+
+        #dispatcher.utter_button_template(trivia, buttons, tracker)
         dispatcher.utter_message(trivia)
-        dispatcher.utter_message(answer)
+        dispatcher.utter_message(buttons = buttons)
+        print(all_answers)
+        # dispatcher.utter_message(trivia)
+        # dispatcher.utter_message(correct_answer)
+        return [SlotSet("correct_answer","{}".format(correct_answer))]
+        #return []
+
+class Correct_Answer:
+
+    def name(self):
+        return "answer_correct_react"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("You answered correctly, great!")
+        return []
+
+
+class Incorrect_Answer:
+
+    def name(self):
+        return "answer_incorrect_react"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("You answered incorrectly, sorry.")
+        return []
+
+class Give_Type(Action):
+
+    def name(self):
+        return "give_type"
+
+    def run(self, dispatcher, tracker, domain):
+        print(tracker.get_slot("trivia_type"))
+        return []
+
+class Verify_Answer(Action):
+    """Function verifying user's answer, comparing it to the correct one and
+    adding one point to score if it is the correct one """
+    def name(self):
+        return "verify_answer"
+
+    def run(self, dispatcher, tracker, domain):
+        print("Users last message was: {}".format(tracker.latest_message.get("text") ) )
+        print("Correct answer is: {}".format(tracker.get_slot("correct_answer")))
+        return[]
+
+class Button_Test(Action):
+
+    def name(self):
+        return "button_test"
+
+    def run(self, dispatcher, tracker, domain):
+        buttons = []
+        buttons.append(
+            {"title": "{}".format("Bye Button"),
+             "payload": "/goodbye"})
+        buttons.append(
+            {"title": "{}".format("Thanks Button"),
+             "payload": "/thanks"}
+        )
+
+        dispatcher.utter_button_template("utter_greet", buttons, tracker)
         return []
